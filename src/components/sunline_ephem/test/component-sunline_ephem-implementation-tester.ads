957aea58cee58960da86dc52435b7c2ec2d29ea0
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-10-30 09:23:58 -0600

    Add Sunline Ephem unit test

---
+++ src/components/sunline_ephem/test/component-sunline_ephem-implementation-tester.ads
@@ -0,0 +1,90 @@
+--------------------------------------------------------------------------------
+-- Sunline_Ephem Component Tester Spec
+--------------------------------------------------------------------------------
+
+-- Includes:
+with Component.Sunline_Ephem_Reciprocal;
+with Printable_History;
+with Data_Product_Return.Representation;
+with Data_Product_Fetch.Representation;
+with Data_Product.Representation;
+with Data_Product;
+with Nav_Att.Representation;
+with Ephemeris;
+with Nav_Trans;
+with Nav_Att;
+
+-- Sunline ephemeris algorithm computes the direction to the sun in the spacecraft
+-- body frame.
+package Component.Sunline_Ephem.Implementation.Tester is
+
+   use Component.Sunline_Ephem_Reciprocal;
+   -- Invoker connector history packages:
+   package Data_Product_Fetch_T_Service_History_Package is new Printable_History (Data_Product_Fetch.T, Data_Product_Fetch.Representation.Image);
+   package Data_Product_Fetch_T_Service_Return_History_Package is new Printable_History (Data_Product_Return.T, Data_Product_Return.Representation.Image);
+   package Data_Product_T_Recv_Sync_History_Package is new Printable_History (Data_Product.T, Data_Product.Representation.Image);
+
+   -- Data product history packages:
+   package Sunline_Body_Frame_History_Package is new Printable_History (Nav_Att.T, Nav_Att.Representation.Image);
+
+   -- Component class instance:
+   type Instance is new Component.Sunline_Ephem_Reciprocal.Base_Instance with record
+      -- The component instance under test:
+      Component_Instance : aliased Component.Sunline_Ephem.Implementation.Instance;
+      -- Connector histories:
+      Data_Product_Fetch_T_Service_History : Data_Product_Fetch_T_Service_History_Package.Instance;
+      Data_Product_T_Recv_Sync_History : Data_Product_T_Recv_Sync_History_Package.Instance;
+      -- Data product histories:
+      Sunline_Body_Frame_History : Sunline_Body_Frame_History_Package.Instance;
+      -- Data dependency return values. These can be set during unit test
+      -- and will be returned to the component when a data dependency call
+      -- is made.
+      Sun_Ephemeris : Ephemeris.T;
+      Spacecraft_Position : Nav_Trans.T;
+      Spacecraft_Attitude : Nav_Att.T;
+      -- The return status for the data dependency fetch. This can be set
+      -- during unit test to return something other than Success.
+      Data_Dependency_Return_Status_Override : Data_Product_Enums.Fetch_Status.E := Data_Product_Enums.Fetch_Status.Success;
+      -- The ID to return with the data dependency. If this is set to zero then
+      -- the valid ID for the requested dependency is returned, otherwise, the
+      -- value of this variable is returned.
+      Data_Dependency_Return_Id_Override : Data_Product_Types.Data_Product_Id := 0;
+      -- The length to return with the data dependency. If this is set to zero then
+      -- the valid length for the requested dependency is returned, otherwise, the
+      -- value of this variable is returned.
+      Data_Dependency_Return_Length_Override : Data_Product_Types.Data_Product_Buffer_Length_Type := 0;
+      -- The timestamp to return with the data dependency. If this is set to (0, 0) then
+      -- the system_Time (above) is returned, otherwise, the value of this variable is returned.
+      Data_Dependency_Timestamp_Override : Sys_Time.T := (0, 0);
+   end record;
+   type Instance_Access is access all Instance;
+
+   ---------------------------------------
+   -- Initialize component heap variables:
+   ---------------------------------------
+   procedure Init_Base (Self : in out Instance);
+   procedure Final_Base (Self : in out Instance);
+
+   ---------------------------------------
+   -- Test initialization functions:
+   ---------------------------------------
+   procedure Connect (Self : in out Instance);
+
+   ---------------------------------------
+   -- Invokee connector primitives:
+   ---------------------------------------
+   -- Fetch a data product item from the database.
+   overriding function Data_Product_Fetch_T_Service (Self : in out Instance; Arg : in Data_Product_Fetch.T) return Data_Product_Return.T;
+   -- The data product invoker connector
+   overriding procedure Data_Product_T_Recv_Sync (Self : in out Instance; Arg : in Data_Product.T);
+
+   -----------------------------------------------
+   -- Data product handler primitives:
+   -----------------------------------------------
+   -- Description:
+   --    Data products for the Sunline Ephem component.
+   -- Sunline direction vector in spacecraft body frame (stored in vehSunPntBdy
+   -- field).
+   overriding procedure Sunline_Body_Frame (Self : in out Instance; Arg : in Nav_Att.T);
+
+end Component.Sunline_Ephem.Implementation.Tester;
---
+++ src/components/sunline_ephem/test/sunline_ephem_tests-implementation.adb
@@ -0,0 +1,109 @@
+--------------------------------------------------------------------------------
+-- Sunline_Ephem Tests Body
+--------------------------------------------------------------------------------
+
+with Basic_Assertions; use Basic_Assertions;
+with Nav_Att;
+with Packed_F32x3.Assertion; use Packed_F32x3.Assertion;
+
+package body Sunline_Ephem_Tests.Implementation is
+
+   -------------------------------------------------------------------------
+   -- Fixtures:
+   -------------------------------------------------------------------------
+
+   overriding procedure Set_Up_Test (Self : in out Instance) is
+   begin
+      -- Allocate heap memory to component:
+      Self.Tester.Init_Base;
+
+      -- Make necessary connections between tester and component:
+      Self.Tester.Connect;
+
+      -- Call component init here.
+      Self.Tester.Component_Instance.Init;
+
+      -- Call the component set up method that the assembly would normally call.
+      Self.Tester.Component_Instance.Set_Up;
+   end Set_Up_Test;
+
+   overriding procedure Tear_Down_Test (Self : in out Instance) is
+   begin
+      -- Free component heap:
+      Self.Tester.Component_Instance.Destroy;
+      Self.Tester.Final_Base;
+   end Tear_Down_Test;
+
+   -------------------------------------------------------------------------
+   -- Tests:
+   -------------------------------------------------------------------------
+
+   -- Run algorithm to ensure integration is sound.
+   overriding procedure Test (Self : in out Instance) is
+      T : Component.Sunline_Ephem.Implementation.Tester.Instance_Access renames Self.Tester;
+
+      -- Test data based on Python test:
+      -- Sun at origin, spacecraft at various positions, identity attitude
+      type Test_Vector is record
+         Sc_Position : Packed_F32x3.T;  -- Spacecraft position
+         Expected_Sunline : Packed_F32x3.T;  -- Expected sunline direction
+      end record;
+
+      Test_Cases : constant array (1 .. 7) of Test_Vector := [
+         (Sc_Position => [-1.0, 0.0, 0.0], Expected_Sunline => [1.0, 0.0, 0.0]),
+         (Sc_Position => [0.0, -1.0, 0.0], Expected_Sunline => [0.0, 1.0, 0.0]),
+         (Sc_Position => [0.0, 0.0, -1.0], Expected_Sunline => [0.0, 0.0, 1.0]),
+         (Sc_Position => [1.0, 0.0, 0.0], Expected_Sunline => [-1.0, 0.0, 0.0]),
+         (Sc_Position => [0.0, 1.0, 0.0], Expected_Sunline => [0.0, -1.0, 0.0]),
+         (Sc_Position => [0.0, 0.0, 1.0], Expected_Sunline => [0.0, 0.0, -1.0]),
+         (Sc_Position => [0.0, 0.0, 0.0], Expected_Sunline => [0.0, 0.0, 0.0])  -- Degenerate case
+      ];
+   begin
+      -- Run each test case
+      for I in Test_Cases'Range loop
+         -- Set sun ephemeris at origin
+         T.Sun_Ephemeris := (
+            R_Bdy_Zero_N => [0.0, 0.0, 0.0],
+            V_Bdy_Zero_N => [0.0, 0.0, 0.0],
+            Sigma_Bn => [0.0, 0.0, 0.0],
+            Omega_Bn_B => [0.0, 0.0, 0.0],
+            Time_Tag => 0.0
+         );
+
+         -- Set spacecraft position to test vector
+         T.Spacecraft_Position := (
+            Time_Tag => 0.0,
+            R_Bn_N => Test_Cases (I).Sc_Position,
+            V_Bn_N => [0.0, 0.0, 0.0],
+            Vehaccumdv => [0.0, 0.0, 0.0]
+         );
+
+         -- Set spacecraft attitude to identity (no rotation)
+         T.Spacecraft_Attitude := (
+            Time_Tag => 0.0,
+            Sigma_Bn => [0.0, 0.0, 0.0],
+            Omega_Bn_B => [0.0, 0.0, 0.0],
+            Veh_Sun_Pnt_Bdy => [0.0, 0.0, 0.0]
+         );
+
+         -- Call algorithm:
+         T.Tick_T_Send ((Time => T.System_Time, Count => 0));
+
+         -- Make sure data product produced:
+         Natural_Assert.Eq (T.Data_Product_T_Recv_Sync_History.Get_Count, I);
+         Natural_Assert.Eq (T.Sunline_Body_Frame_History.Get_Count, I);
+
+         -- Check the sunline output (stored in vehSunPntBdy field)
+         declare
+            Output : constant Nav_Att.T := T.Sunline_Body_Frame_History.Get (I);
+         begin
+            Packed_F32x3_Assert.Eq (
+               Output.Veh_Sun_Pnt_Bdy,
+               Test_Cases (I).Expected_Sunline,
+               Epsilon => 0.0001
+            );
+         end;
+      end loop;
+   end Test;
+
+end Sunline_Ephem_Tests.Implementation;
