567ed55aa1f3c35de6b49faa1ccde8d78cbc8033
Kevin Dinkel <1225857+dinkelk@users.noreply.github.com>
2025-11-03 08:17:03 -0700

    Remove license from bindings
    File in this repo are licensed under the global LICENSE in the repo
    root.

--- src/components/attitude_tracking_error/att_tracking_error_algorithm_c.ads
+++ src/components/attitude_tracking_error/att_tracking_error_algorithm_c.ads
@@ -11,26 +11,6 @@ with Packed_F32x3_Record.C;
 
 package Att_Tracking_Error_Algorithm_C is
 
-   -- ISC License
-   -- *
-   -- * Copyright (c) 2025, Laboratory for Atmospheric and Space Physics,
-   -- * University of Colorado at Boulder
-   -- *
-   -- * Permission to use, copy, modify, and/or distribute this software
-   -- * for any purpose with or without fee is hereby granted, provided
-   -- * that the above copyright notice and this permission notice appear
-   -- * in all copies.
-   -- *
-   -- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
-   -- * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
-   -- * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
-   -- * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
-   -- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
-   -- * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
-   -- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
-   -- * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-   --
-
    --* Opaque handle for an AttTrackingErrorAlgorithm instance.
    type Att_Tracking_Error_Algorithm is limited private;
    type Att_Tracking_Error_Algorithm_Access is access all Att_Tracking_Error_Algorithm;
